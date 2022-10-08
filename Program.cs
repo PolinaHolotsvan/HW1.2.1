f594f42a5000b359575918d60e7df72df66684b5
lenovo <lenovo@DESKTOP-FJ0HVGL>
2022-10-09 01:19:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿double N;
+bool converted = false;
+
+Console.WriteLine("Введiть число");
+do
+{
+    string line = Console.ReadLine();
+    if (line == "")
+    {
+        Console.WriteLine("Ви не ввели число, введiть знову!");
+    }
+    else
+    {
+        try
+        {
+            N = double.Parse(line);
+            Console.WriteLine($"Ви ввели число {N}");
+            converted = true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ви неправильно ввели число, введiть знову!");
+        }
+    }
+}
+while (!converted);
+
+Console.ReadKey();
